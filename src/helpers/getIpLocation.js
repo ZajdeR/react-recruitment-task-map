785ac50c6f6f030a62bff6fe6f046602cc49924b
Pawel <pawelzajder@gmail.com>
2021-01-06 12:22:10 +0100

    getIpLocation.js - change to https

--- src/helpers/getIpLocation.js
+++ src/helpers/getIpLocation.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 export const getIpLocation = async (ip) => {
   const APIKEY = '56afd34d62f03d50f12dbff15126a0b0'; // TODO: move to .env
 
-  return await axios.get(`http://api.ipstack.com/${ip}?access_key=${APIKEY}`);
+  return await axios.get(`https://api.ipstack.com/${ip}?access_key=${APIKEY}`);
 };
