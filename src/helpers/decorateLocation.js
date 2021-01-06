09b43b167bad96c41b49d50ba9788277c6d974a4
Pawel <pawelzajder@gmail.com>
2021-01-06 12:26:32 +0100

    getIpLocation.js - change to https / revert, needed subscription plan for using https

--- src/helpers/decorateLocation.js
+++ src/helpers/decorateLocation.js
@@ -1,4 +1,8 @@
 export const decorateLocation = (location) => {
+  if (!location.data) {
+    return null;
+  }
+
   const { city, country_name, continent_name, region_name } = location.data;
 
   return `${city}, ${region_name}, ${country_name}, ${continent_name}`;
