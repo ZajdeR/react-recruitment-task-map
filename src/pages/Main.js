164aae035fe228541a7256892409289cceb26471
Pawel <pawelzajder@gmail.com>
2021-01-09 10:02:28 +0100

    Main.js - rework to hooks

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,79 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ListSearch from '../components/ListSearch';
 import Map from '../components/Map';
 import BoxInformation from '../components/BoxInformation';
-import BoxSearch from '../components/BoxSearch';
+import FormSearch from '../components/FormSearch';
 import './Main.css';
 import { getClientIp } from '../helpers/getClientIp';
 import { getIpLocation } from '../helpers/getIpLocation';
 import { decorateLocation } from '../helpers/decorateLocation';
 
-export default class Main extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userIp: null,
-      userLocation: null,
-      searches: [],
-      searchText: '',
-    };
+function Main() {
+  const [userIp, setUserIp] = useState(null);
+  const [userLocation, setUserLocation] = useState(null);
+  const [searches, setSearches] = useState([]);
+  const [searchText, setSearchText] = useState('');
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSearch = this.handleSearch.bind(this);
-  }
-
-  async componentDidMount() {
+  const detectUserLocation = async () => {
     const detectedIp = await getClientIp();
     const detectedLocation = await getIpLocation(detectedIp);
 
-    this.setState({
-      userIp: detectedIp,
-      userLocation: decorateLocation(detectedLocation),
-    });
-  }
+    setUserIp(detectedIp);
+    setUserLocation(decorateLocation(detectedLocation));
+  };
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    const detectLocation = await getIpLocation(searchText);
+    const newSearches = [
+      {
+        address: decorateLocation(detectLocation),
+        ip: searchText,
+      },
+    ].concat(searches);
 
-  handleChange(event) {
-    this.setState({ searchText: event.target.value });
-  }
+    setSearches(newSearches);
+  };
 
-  async handleSearch(event) {
-    event.preventDefault();
-    const detectLocation = await getIpLocation(this.state.searchText);
+  useEffect(detectUserLocation);
 
-    this.setState((state) => {
-      const searches = [
-        {
-          address: decorateLocation(detectLocation),
-          ip: state.searchText,
-        },
-      ].concat(state.searches);
-
-      return { searches };
-    });
-  }
-
-  render() {
-    return (
-      <div className="grid grid-cols-3 h-screen">
-        <ListSearch searches={this.state.searches} />
-        <Map />
-        <BoxInformation
-          ip={this.state.userIp}
-          label="User data"
-          location={this.state.userLocation}
-        />
-        <BoxSearch
-          searchMethod={this.handleSearch}
-          searchText={this.state.searchText}
-          changeMethod={this.handleChange}
-        />
-        <Map />
-        <BoxInformation
-          label="Last search data"
-          ip={this.state.searches[0]?.ip}
-          location={this.state.searches[0]?.address}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className="grid grid-cols-3 h-screen">
+      <ListSearch searches={searches} />
+      <Map />
+      <BoxInformation
+        label="User location"
+        ip={userIp}
+        location={userLocation}
+      />
+      <FormSearch
+        searchMethod={handleSearch}
+        searchText={searchText}
+        changeMethod={(e) => setSearchText(e.target.value)}
+      />
+      <Map />
+      <BoxInformation
+        label="Last search location"
+        ip={searches[0]?.ip}
+        location={searches[0]?.address}
+      />
+    </div>
+  );
 }
+
+export default Main;
